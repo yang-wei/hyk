ba1b3dd1e503b90f18c7bebe49dca754146476d9
yang-wei <yangweilim21@gmail.com>
2015-05-24 23:44:17 +0900

    Add usage information

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,8 +5,17 @@ require("babel/register");
 /**
  * Module dependencies
  */
+var fs = require('fs');
 var hyk = require('../lib');
 var source = require('../lib/api_source');
 var wordArg = process.argv[2];
+
+if(!wordArg) {
+  fs.readFile(__dirname + '/help.txt', 'utf8', function(err, data) {
+    if(err) throw err;
+    console.log(data);
+    process.exit(1);
+  }); 
+}
 
 hyk(wordArg, source);
