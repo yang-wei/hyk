b716b3a986b4d63c82995ce41b0dfc583ef8b214
yang-wei <yangweilim21@gmail.com>
2015-05-24 22:59:54 +0900

    es6 rocks

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+require("babel/register");
+
 /**
  * Module dependencies
  */
 var hyk = require('../lib');
+var source = require('../lib/api_source');
 var wordArg = process.argv[2];
 
-hyk(wordArg);
+hyk(wordArg, source);
